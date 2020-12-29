b9a9e2a998d1d9fefbee2a021ce318971e077346
arek <arkadiusz.kar96@gmail.com>
2020-12-29 01:37:58 +0100

    2912g0137

--- app/src/main/java/com/example/cyprjakdojade/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/cyprjakdojade/ui/home/HomeFragment.java
@@ -2,6 +2,7 @@ package com.example.cyprjakdojade.ui.home;
 
 import android.os.Bundle;
 import android.text.Editable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,11 +17,22 @@ import androidx.lifecycle.ViewModelProvider;
 import android.widget.Button;
 
 import com.example.cyprjakdojade.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class HomeFragment extends Fragment {
 
+    private static final String TAG = "blad";
     private HomeViewModel homeViewModel;
     EditText tv;
+    EditText tv2;
+    String name;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -34,14 +46,37 @@ public class HomeFragment extends Fragment {
                 textView.setText(s);
             }
         });
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference ref = database.getReference("name");
+        //reference = FirebaseDatabase.getInstance().getReference("users").child(id);
+        // tu wklejam
+        ValueEventListener valueEventListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                name = dataSnapshot.getValue(String.class);
+                Log.d(TAG, name);
+                System.out.println("IMIE: "+name);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                System.out.println("BLAAAAAD");
+            }
+        };
+        ref.addListenerForSingleValueEvent(valueEventListener);
+
         tv = root.findViewById(R.id.EditTextSearch);
+        tv2 = root.findViewById(R.id.EditTextSearch2);
         Button button = (Button)root.findViewById(R.id.buttonSearch);
         button.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
-                String doWyszukania = tv.getText().toString();
-                System.out.println(doWyszukania);
-                System.out.println("NAPIS NAPIS");
+                System.out.println("----------------NAPIS NAPIS-----------------");
+                String przystanekPoczatkowy = tv.getText().toString();
+                System.out.println("poczatkowy: "+przystanekPoczatkowy);
+                String przystanekKoncowy = tv2.getText().toString();
+                System.out.println("koncowy: "+przystanekKoncowy);
+                System.out.println("CZY DZIALA IMIE?: "+name);
             }
         });
         return root;
